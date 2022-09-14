e293aaaf15b4ea0792a506133429a589f26f6a2c
Mateusz <kroplewskimateusz@gmail.com>
2022-09-14 11:50:32 +0100

    designed front page

--- index.php
+++ index.php
@@ -2,6 +2,23 @@
     include 'config.php';
 ?>
 <body>
-    
+    <section class="w-[100vw]">
+         <div class="md:w-[40%] w-[100%] md:h-[100vh] h-[300px] md:float-right md:bg-purple-800/50">
+            <img src="./assets/mat.png" alt="" class="md:w-[500px] w-[100px] h-[100px] md:h-[500px] relative z-10 rounded-full mx-auto mt-[100px] border-solid border-2 border-zinc-300">
+        </div>
+
+        <div class="md:w-[60%] w-[100%] h-[100vh] md:float-left text-center text-zinc-300">
+            <p class="text-[60px] font-bold mt-16">Welcome to</p>
+            <p class="text-[50px] font-light mt-5">talk to Mat</p>
+
+        <div class="md:w-[500px] w-[100%] mx-auto mt-[100px]">
+            <p class="text-left font-semibold">please start with:</p>
+            <button class="w-[300px] h-[50px] bg-purple-800 font-bold rounded-md mt-5">Registering Account</button>
+
+            <p class="text-left font-semibold mt-10">Or:</p>
+            <button class="w-[300px] h-[50px] bg-purple-800 font-bold rounded-md mt-5">Log into account</button>
+        </div>
+        </div>
+   </section>
 </body>
 </html>
